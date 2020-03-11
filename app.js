ff243b4e35936a5ad5e3067b5229da9fd2bd222f
Mourtaza <mourtaza.abbasbhay@ecl18.ec-lyon.fr>
2020-03-11 11:42:12 +0100

    server update

--- app.js
+++ app.js
@@ -1,14 +1,56 @@
+// --------------
+// -- includes --
+// --------------
+
 const express = require('express')
 const app = express()
+const path = require('path')
 const cookieParser = require('cookie-parser')
-
-const port = 3000
+const bodyParser = require('body-parser')
+const io = require('socket.io')()
 
 // cookieParser
 app.use(cookieParser())
 
+// bodyParser
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
 // set public path
 app.use(express.static('public'))
+
+// login page default
+app.get('/', function (req, res) {
+  res.redirect('login.html')
+})
+
+// -----------
+// -- Login --
+// -----------
+
+app.get('/login', function (req, res) {
+  console.log(1)
+})
+
+app.post('/login', function (req, res) {
+  console.log(JSON.stringify(req.body))
+  const username = req.body.username
+  const password = req.body.password
+  if (username === 'user' && password === 'pass') {
+    res.redirect('lobby.html')
+  } else {
+    res.redirect('/')
+  }
+})
+
+// -----------
+// -- Users --
+// -----------
+
+app.get('/users', function (req, res) {
+  const clients = io.clients()
+  res.send(clients)
+})
 
 // -----------------
 // -- HTTP errors --
@@ -17,15 +59,19 @@ app.use(express.static('public'))
 // handle 404
 app.use(function (err, req, res, next) {
   console.log(err)
-  res.status(404).sendFile('/html/error-404.html')
+  res.status(404).sendFile('error-404.html')
 })
 
 // handle 500
 app.use(function (err, req, res, next) {
   console.log(err)
-  res.status(500).sendFile('/html/error-500.html')
+  res.status(500).sendFile('error-500.html')
 })
 
-app.listen(port, function () {
-  console.log('Server started')
+// ------------------
+// -- Start server --
+// ------------------
+
+app.listen(3000, function () {
+  console.log('\nServer started, listening on PORT 3000')
 })
