ec818496080f6e75723183310cc597ea76cb1702
Mourtaza <mourtaza.abbasbhay@ecl18.ec-lyon.fr>
2020-04-09 15:07:28 +0200

    heroku

--- public/lobby.js
+++ public/lobby.js
@@ -1,15 +1,18 @@
-  $(function () {
-    const socket = io();
+/* global $ */
+/* global io */
 
-    // send message
-    $('form').submit(function(e){
-      e.preventDefault()
-      socket.emit('nouveau message', $('#msg').val())
-      $('#msg').val('')
-    })
+$(function () {
+  const socket = io()
 
-    // receive message
-    socket.on('reponse', function(msg){
-      $('#messages').append($('<li>').text(msg))
-    })
+  // send message
+  $('form').submit(function (e) {
+    e.preventDefault()
+    socket.emit('nouveau message', $('#msg').val())
+    $('#msg').val('')
   })
+
+  // receive message
+  socket.on('reponse', function (msg) {
+    $('#messages').append($('<li>').text(msg))
+  })
+})
